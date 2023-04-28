d37d65a894e63fb69fdad0d4fea992cbc58b0ab3
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-28 11:41:16 +0100

    set uuid to contact array list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 import Header from './pages/Header'
 import AddPerson from './pages/AddPerson'
 import BirthdayList from './pages/BirthdayList'
 
 const App = () => {
-const [user, setUser] = useState('')
+const [user, setUser] = useState([])
 
 const addPerson = (person) => {
-  setUser(person)
+  setUser([...user, {id: uuidv4(), ...person}])
 }
 
 
