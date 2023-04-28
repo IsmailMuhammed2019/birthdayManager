b9c35ec3856b2493c1ea30eb4e4e8d800da13472
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-28 11:25:25 +0100

    set card layout

--- src/pages/BirthdayList.jsx
+++ src/pages/BirthdayList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const BirthdayList = () => {
+const BirthdayList = ({user}) => {
   return (
-    <div>BirthdayList</div>
+    <div className="ui celled list">
+        <div className="ui item" style={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
+            <div className="content">
+                <h3 className="header">
+                    Name: {name}
+                </h3>
+                <p>
+                    Email: {email}
+                </p>
+                <p>
+                    Date: {date}
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
