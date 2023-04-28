6f474ef7f01602063e9b25f71684c728e770a594
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-28 10:58:00 +0100

    create addperson function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 import Header from './pages/Header'
 import AddPerson from './pages/AddPerson'
 
 const App = () => {
+const [user, setUser] = useState('')
+
+const addPerson = (person) => {
+  setUser(person)
+}
+
   return (
     <div>
       <Header />
       <div className="ui container">
-        <AddPerson />
+        <AddPerson addPerson={addPerson} />
       </div>
     </div>
   )
