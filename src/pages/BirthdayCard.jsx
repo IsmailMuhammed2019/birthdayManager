f7dcfdc56ca169af9496dba2ea49b1c62b2e5a8d
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-04-28 16:28:43 +0100

    handleclick id passed

--- src/pages/BirthdayCard.jsx
+++ src/pages/BirthdayCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const BirthdayCard = () => {
+const BirthdayCard = ({person, handleClick}) => {
+    const { id, name, email, date} = person
   return (
-    <div>BirthdayCard</div>
+    <div className="ui item" style={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
+            <div className="content">
+                <h3 className="header">
+                    {name}
+                </h3>
+                <p className='header'>
+                    {email}
+                </p>
+                <p className='header'>
+                    {date}
+                </p>
+            </div>
+            <i className="trash alternate outline icon red style={{cursor: 'pointer'}}"  onClick={() => handleClick(id)}/>
+        </div>
   )
 }
 
